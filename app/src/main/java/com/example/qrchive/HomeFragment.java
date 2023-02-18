5ae0a007ed4f4e4975ddbdad88e1d5097c4d523d
Justin Meimar <j.meimar111@gmail.com>
2023-02-18 15:43:08 -0700

    bottom nav complete

--- app/src/main/java/com/example/qrchive/HomeFragment.java
+++ app/src/main/java/com/example/qrchive/HomeFragment.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * create an instance of this fragment.
@@ -29,8 +30,8 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
