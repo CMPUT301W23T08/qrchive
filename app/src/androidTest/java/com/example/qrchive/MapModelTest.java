0841bdf20155b30b5a3da6ede9259ad56ea91d40
Rana Sunjog Singh Thind <71564731+sunjogthind@users.noreply.github.com>
2023-03-13 02:49:48 -0600

    Updated MapModelTest
    wrote docs and removed un-necessary imports.

--- app/src/androidTest/java/com/example/qrchive/MapModelTest.java
+++ app/src/androidTest/java/com/example/qrchive/MapModelTest.java
@@ -1,33 +1,45 @@
 package com.example.qrchive;
 
 
-import org.imperiumlabs.geofirestore.GeoFirestore;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
 import com.example.qrchive.Classes.MapModel;
 import com.example.qrchive.Classes.ScannedCode;
 import com.example.qrchive.Classes.onCodesGeoQueriedListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.GeoPoint;
-
-import java.util.List;
 
 
-import org.junit.Test;
-import static org.junit.Assert.*;
 
+/**
+ This test file is for testing the functionality of the MapModel class.
+ It contains two test cases:
+ testGetNearbyQRCodes() - tests that the getNearbyQRCodes() method returns an empty list when no nearby QR codes are found.
+ testSetNearbyQRCodes() - tests that the setNearbyQRCodes() method calls the onCodesGeoQueried() callback function when a nearby QR code is found.
+ */
 public class MapModelTest {
+
+    /**
+     * This test case tests that the getNearbyQRCodes() method returns an empty list when no nearby QR codes are found.
+     */
 
     @Test
     public void testGetNearbyQRCodes() {
+        // Create a new MapModel object with (0,0) coordinates
         MapModel mapModel = new MapModel(0, 0);
+
+        // Assert that the list of nearby QR codes is empty
         assertTrue(mapModel.getNearbyQRCodes().isEmpty());
     }
 
+
+    /**
+     * This test case tests that the setNearbyQRCodes() method calls the onCodesGeoQueried() callback function when a nearby QR code is found.
+     */
+
     @Test
     public void testSetNearbyQRCodes() {
+        // Create a new MapModel object with (0,0) coordinates
         MapModel mapModel = new MapModel(0, 0);
+        // Call setNearbyQRCodes() with a new onCodesGeoQueriedListener object
         mapModel.setNearbyQRCodes(new onCodesGeoQueriedListener() {
             @Override
             public void onCodesGeoQueried(ScannedCode scannedCode) {
